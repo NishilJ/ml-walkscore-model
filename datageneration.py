a458b5c0c951f85ff9ade9c0ff5c228b8c914103
NishilJ <78994772+NishilJ@users.noreply.github.com>
2025-04-16 18:12:43 -0500

    fixes

--- datageneration.py
+++ datageneration.py
@@ -7,6 +7,9 @@ from shapely.geometry import Point
 from popdensity import get_pop_density
 from osmfeatures import get_osm_features
 
+data_points = 50 # Total amount of coord data points to generate
+radius = 500 # Find OSM features in a radius (meters) around each coord
+
 def get_random_us_coord():
     #us_boundary = gpd.read_file("boundaries/us/ne_110m_admin_0_countries.shp")
     #us_boundary = us_boundary[us_boundary['ADMIN'] == 'United States of America']
@@ -24,13 +27,13 @@ def get_random_us_coord():
             return round(lat, 4), round(lon, 4)
 
 async def main():
-    coords_list = [get_random_us_coord() for _ in range(15)] # TOTAL AMOUNT OF COORD ENTRIES
+    coords_list = [get_random_us_coord() for _ in range(data_points)] # TOTAL AMOUNT OF COORD ENTRIES
 
     print("Calculating population densities...")
     pop_densities = await asyncio.gather(*[get_pop_density(coords) for coords in coords_list])
     print("Population densities calculated!")
 
-    file_path = "data.txt"
+    file_path = "data.csv"
     header = ['Lat', 'Lon', 'Pop Density', 'Intersections', 'Pedways', 'Bikeways', 'POIs', 'Transit', 'Retail']
     with open(file_path, mode='w', newline='') as f:
         writer = csv.writer(f, delimiter=',')
@@ -41,8 +44,8 @@ async def main():
 
         for i, coords in enumerate(coords_list):
             pop_density = pop_densities[i]
-            print(f"Calculating OSM feature densities at ({coords})...")
-            osm_features = get_osm_features(coords, 1000)
+            print(f"#{i + 1} Calculating OSM feature densities at ({coords})...")
+            osm_features = get_osm_features(coords, radius)
 
             row = [coords[0], coords[1], pop_density,
                          osm_features['intersections'],
@@ -54,7 +57,7 @@ async def main():
 
             writer.writerow(row)
             f.flush()
-            print(f"#{i + 1} {coords} data written to {file_path}.")
+            print(f"#{i + 1} {coords} row written to {file_path}.")
 
     print(f"Data generation complete at {file_path}.")
 
--- osmfeatures.py
+++ osmfeatures.py
@@ -1,5 +1,7 @@
+
 import osmnx as ox
 import numpy as np
+import pandas as pd
 from geopandas import GeoDataFrame
 
 
@@ -19,10 +21,10 @@ def get_osm_data(coords: tuple[float, float], radius: int, tags: dict):
     try:
         return ox.features.features_from_point(coords, dist=radius, tags=tags)
     except:
-        return None
+        return {}
 
 
-def calculate_density(gdf : GeoDataFrame, radius: int) :
+def calculate_density(gdf : pd.DataFrame, radius: int) :
     """
     Calculate the density of features per square kilometer.
 
@@ -33,18 +35,31 @@ def calculate_density(gdf : GeoDataFrame, radius: int) :
     Returns:
         float: Density of features per square kilometer.
     """
-    if gdf is None or  radius == 0:
+    if gdf is None or radius == 0:
         return 0.00
     return round(len(gdf) * 1e6 / (np.pi * radius**2), 2)
 
 def get_osm_features(coords: tuple[float, float], radius: int):
+    tags = {
+        'highway': ['footway', 'cycleway', 'crossing', 'stop', 'traffic_signals'],
+        'amenity': True,
+        'public_transport': True,
+        'landuse': ['retail'],
+        'cycleway': True
+    }
+
+    feature_data = get_osm_data(coords, radius, tags=tags)
     features = {
-        'intersections': get_osm_data(coords, radius, tags={'highway': ['stop', 'traffic_signals']}),
-        'pedways': get_osm_data(coords, radius, tags={'highway': ['footway', 'crossing']}),
-        'bikeways': get_osm_data(coords, radius, tags={'highway': ['cycleway'], 'cycleway': True}),
-        'pois': get_osm_data(coords, radius, tags={'amenity': True}),
-        'transit_stops': get_osm_data(coords, radius, tags={'public_transport': True}),
-        'retail': get_osm_data(coords, radius, tags={'landuse': ['retail']})
+        'intersections': feature_data[feature_data['highway'].isin(['stop', 'traffic_signals'])] if 'highway' in feature_data else None,
+        'pedways': feature_data[feature_data['highway'].isin(['footway', 'crossing'])] if 'highway' in feature_data else None,
+        'bikeways':
+                pd.concat([
+                    feature_data[feature_data['highway'].isin(['cycleway'])] if 'highway' in feature_data else pd.DataFrame(),
+                    feature_data[feature_data['cycleway'].notna()] if 'cycleway' in feature_data else pd.DataFrame()
+                ], ignore_index=True),
+        'pois': feature_data[feature_data['amenity'].notna()] if 'amenity' in feature_data else None,
+        'transit_stops': feature_data[feature_data['public_transport'].notna()] if 'public_transport' in feature_data else None,
+        'retail': feature_data[feature_data['landuse'].isin(['retail'])] if 'landuse' in feature_data else None
     }
 
     densities = {}
@@ -54,7 +69,11 @@ def get_osm_features(coords: tuple[float, float], radius: int):
     return densities
 
 # coords = (lat, long)
-#coords = (40.7128, -74.0060)  # New York City
-#coords2 = (33.0786, -96.7047)  # Home Plano
-#coords2 = (42.3563, -71.0588)
+coords4 = (40.7128, -74.0060)  # New York City
+coords1 = (33.0786, -96.7047)  # Home Plano
+coords2 = (42.3563, -71.0588)
+coords3 = (33.360763, -100.110555)
 
+#result = get_osm_features((40.9075, -74.2526), 1000)
+
+#print(result)
