8fb7abe693e814a6956124896900fb5851f0e748
Syed Kabir <syedkabir909@gmail.com>
2025-04-26 23:50:14 -0500

    Added the DecisionTreeRegressor model code

--- Models.py
+++ Models.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
@@ -32,25 +33,64 @@ X_test = scaler.transform(X_test)
 
 joblib.dump(scaler, 'scaler.pkl')
 
-# training the model
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+# training the RandomForestRegressor model
+model_rfr = RandomForestRegressor(
+    n_estimators=236,
+    max_depth=4,
+    min_samples_leaf=5,
+    min_samples_split=23,
+    max_features='sqrt',
+    random_state=42,
+)
+model_rfr.fit(X_train, y_train)
 
 # test prediction
-y_pred = model.predict(X_test)
+y_pred = model_rfr.predict(X_test)
 
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+mae_rfr = mean_absolute_error(y_test, y_pred)
+mse_rfr = mean_squared_error(y_test, y_pred)
+r2_rfr = r2_score(y_test, y_pred)
 
-print(f"Mean Absolute Error (MAE): {mae}")
-print(f"Mean Squared Error (MSE): {mse}")
-print(f"R2 Score: {r2}\n")
+print(f"Mean Absolute Error (MAE): {mae_rfr}")
+print(f"Mean Squared Error (MSE): {mse_rfr}")
+print(f"R2 Score: {r2_rfr}\n")
 
-feature_importances = model.feature_importances_
+feature_importances = model_rfr.feature_importances_
 print("Feature Importances:")
 for feature, importance in zip(feature_labels, feature_importances):
     print(f"{feature}: {importance:.4f}")
 
-joblib.dump(model, 'RandomForestRegressorModel.pkl')
-print("Model saved as RandomForestRegressorModel.pkl")
+joblib.dump(model_rfr, 'RandomForestRegressorModel.pkl')
+print("Model saved as RandomForestRegressorModel.pkl\n")
+
+print(50*"-")
+
+# training the DecisionTreeRegressor
+model_dtr = DecisionTreeRegressor(
+    max_depth=5,
+    min_samples_leaf=5,
+    min_samples_split=10,
+    max_features='sqrt',
+    random_state=42
+)
+
+model_dtr.fit(X_train, y_train)
+
+# test prediction
+y_pred = model_dtr.predict(X_test)
+
+mae_dtr = mean_absolute_error(y_test, y_pred)
+mse_dtr = mean_squared_error(y_test, y_pred)
+r2_dtr = r2_score(y_test, y_pred)
+
+print(f"Mean Absolute Error (MAE): {mae_dtr}")
+print(f"Mean Squared Error (MSE): {mse_dtr}")
+print(f"R2 Score: {r2_dtr}\n")
+
+feature_importances_dtr = model_dtr.feature_importances_
+print("Feature Importances:")
+for feature, importance in zip(feature_labels, feature_importances_dtr):
+    print(f"{feature}: {importance:.4f}")
+
+joblib.dump(model_dtr, 'DecisionTreeRegressorModel.pkl')
+print("Model saved as DecisionTreeRegressorModel.pkl")
